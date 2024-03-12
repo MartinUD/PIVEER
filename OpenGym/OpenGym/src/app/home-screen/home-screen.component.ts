f1d511b4b16d6b20789c2ceac5aa0d167af09a41
MartinUD <martin.ud123@gmail.com>
2024-03-12 14:20:52 +0100

    Added OpenGym

---
+++ OpenGym/OpenGym/src/app/home-screen/home-screen.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-screen.component.html',
+  styleUrl: './home-screen.component.css'
+})
+export class HomeScreenComponent implements OnInit{
+
+   constructor() { }
+   
+   ngOnInit(): void {
+
+   }
+
+}
